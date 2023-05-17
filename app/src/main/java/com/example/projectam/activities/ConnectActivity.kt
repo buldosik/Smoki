3aa976787820e086f7e53ea037f80b29f6d125c4
Maksym Lukianenko <maks.lukyanenko195@gmail.com>
2023-05-17 19:35:12 +0200

    Added functionality for ConnectActivity

--- app/src/main/java/com/example/projectam/activities/ConnectActivity.kt
+++ app/src/main/java/com/example/projectam/activities/ConnectActivity.kt
@@ -3,13 +3,18 @@ package com.example.projectam.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectam.R
 
 class ConnectActivity : AppCompatActivity() {
+    private lateinit var romeCodeET: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.connect_activity)
+
+        romeCodeET = findViewById(R.id.roomCodeET)
     }
 
     fun connectToLobby(view: View) {
